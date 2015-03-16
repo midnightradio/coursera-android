6466489adb4c94bb21f213fc65b36d5467ca49bd
Hongjoo Lee <mamadontgodaddycomehome@gmail.com>
2015-03-17 02:16:02 +0800

    Lab3 Permission submitting

--- Labs/Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
+++ Labs/Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
@@ -79,6 +79,8 @@ public class BookmarksActivity extends Activity {
 		Log.i(TAG, "Entered startGoToDangerousActivity()");
 
 		// TODO - Start the GoToDangerousActivity
+		Intent dangerousIntent = new Intent(this, GoToDangerousActivity.class);
+		startActivity(dangerousIntent);
 		
 
 	}
